50d3da7c416a6e10fed1d8a7661f173f6e2ffcc2
Jonas Hals <contact@jonashals.me>
2020-02-04 11:21:40 +0100

    Make example get latest ETH/USD price (#8)

--- substrate-node-example/runtime/src/example.rs
+++ substrate-node-example/runtime/src/example.rs
@@ -4,7 +4,7 @@ use chainlink::{Event, create_request_event_from_parameters};
 use sp_std::prelude::*;
 
 pub trait Trait: system::Trait {
-	type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
+    type Event: From<Event<Self>> + Into<<Self as system::Trait>::Event>;
 }
 
 decl_storage! {
@@ -15,21 +15,22 @@ decl_storage! {
 
 decl_module! {
     pub struct Module<T: Trait> for enum Call where origin: T::Origin {
-		fn deposit_event() = default;
+        fn deposit_event() = default;
 
-		pub fn send_request(origin) -> DispatchResult {
-            // TODO Investigate if Enum can be safely used to refer to a callback 
+        pub fn send_request(origin) -> DispatchResult {
+            // TODO Investigate if Enum can be safely used to refer to a callback
             // let name: &str = stringify!(Call::<T>::callback); 'Example :: callback'
             // For now , simply rely on a string to identify the callback
             let who : <T as system::Trait>::AccountId = ensure_signed(origin)?;
-			Self::deposit_event(create_request_event_from_parameters::<T, (&[u8], &[u8], &[u8], &[u8], &[u8], &[u8])>(1, 0, who, 0, (b"get", b"https://min-api.cryptocompare.com/data/pricemultifull?fsyms=ETH&tsyms=USD", b"path", b"RAW.ETH.USD.CHANGEPCTDAY", b"times", b"2"), "Example.callback".into()));
-			Ok(())
+            Self::deposit_event(create_request_event_from_parameters::<T, (&[u8], &[u8], &[u8], &[u8], &[u8], &[u8])>(1, 0, who, 0, (b"get", b"https://min-api.cryptocompare.com/data/pricemultifull?fsyms=ETH&tsyms=USD", b"path", b"RAW.ETH.USD.PRICE", b"times", b"100000000"), "Example.callback".into()));
+            Ok(())
         }
-        
+
         pub fn callback(origin, result: u128) -> DispatchResult {
             let who : <T as system::Trait>::AccountId = ensure_signed(origin)?;
             <Result>::put(result);
-			Ok(())
-		}
+            Ok(())
+        }
     }
 }
+
