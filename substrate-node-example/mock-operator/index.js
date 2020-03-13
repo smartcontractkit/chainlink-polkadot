891a7802297ba909659dbef1239d54a0cff4b87f
Julien Eluard <julien.eluard@gmail.com>
2020-03-13 16:44:08 +0100

    Make sure script works the first time

--- substrate-node-example/mock-operator/index.js
+++ substrate-node-example/mock-operator/index.js
@@ -3,6 +3,41 @@ const { cryptoWaitReady }  = require('@polkadot/util-crypto');
 
 const PHRASE = 'entire material egg meadow latin bargain dutch coral blood melt acoustic thought';
 
+async function fundOperatorAccountIfNeeded(api, aliceAccount, operatorAccount) {
+  // TODO migrate to 'system.account' ? See https://github.com/paritytech/substrate/pull/4820
+  return new Promise(async (resolve) => {
+    const balance = await api.query.balances.freeBalance(operatorAccount.address);
+    if (balance.isZero()) {
+        await api.tx.balances.transfer(operatorAccount.address, 123456666).signAndSend(aliceAccount, async ({ status }) => {
+          if (status.isFinalized) {
+            resolve();
+          }
+        });
+        // TODO make sure we wait for block being included
+        console.log('Operator funded');
+    } else {
+      resolve();
+    }
+  });
+}
+
+async function registerOperatorIfNeeded(api, operatorAccount) {
+  // Register the operator, this is supposed to be initiated once by the operator itself
+  return new Promise(async (resolve) => {
+    const operator = await api.query.chainlink.operators(operatorAccount.address);
+    if(operator.isFalse) {
+        await api.tx.chainlink.registerOperator().signAndSend(operatorAccount, async ({ status }) => {
+          if (status.isFinalized) {
+            console.log('Operator registered');
+            resolve();
+          }
+        });
+    } else {
+      resolve();
+    }
+  });
+}
+
 async function main() {
     await cryptoWaitReady();
 
@@ -24,19 +59,8 @@ async function main() {
 
     // Make sure this operator has some funds
     const aliceAccount = keyring.addFromUri('//Alice');
-    const balance = await api.query.balances.freeBalance(operatorAccount.address);
-    if (balance.isZero()) {
-        await api.tx.balances.transfer(operatorAccount.address, 123456666).signAndSend(aliceAccount);
-        // TODO make sure we wait for block being included
-        console.log('Operator funded');
-    }
 
-    // Register the operator, this is supposed to be initiated once by the operator itself
-    const operator = await api.query.chainlink.operators(operatorAccount.address);
-    if(operator.isFalse) {
-        await api.tx.chainlink.registerOperator().signAndSend(operatorAccount);
-        console.log('Operator registered');
-    }
+    await fundOperatorAccountIfNeeded(api, aliceAccount, operatorAccount);
 
     const result = await api.query.example.result();
     console.log(`Result is currently ${result}`);
@@ -53,12 +77,15 @@ async function main() {
                 if (status.isFinalized) {
                   const updatedResult = await api.query.example.result();
                   console.log(`Result is now ${updatedResult}`);
+                  process.exit();
                 }
               });
             console.log(`Operator answered to request ${id} with ${value}`);
         }
       });
     });
+
+    await registerOperatorIfNeeded(api, operatorAccount);
 
     // Then simulate a call from alice
     await api.tx.example.sendRequest(operatorAccount.address).signAndSend(aliceAccount);
