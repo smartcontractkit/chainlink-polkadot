8ad333a2c04030f69736e5fb4651ce1ca4249a60
Matthias Seitz <matthias.seitz@outlook.de>
2021-10-31 14:18:19 +0100

    chore(deps): update to polkadot-v0.9.12

--- substrate-node-example/node/src/service.rs
+++ substrate-node-example/node/src/service.rs
@@ -190,6 +190,7 @@ pub fn new_full(mut config: Configuration) -> Result<TaskManager, ServiceError> 
 	let warp_sync = Arc::new(sc_finality_grandpa::warp_proof::NetworkProvider::new(
 		backend.clone(),
 		grandpa_link.shared_authority_set().clone(),
+		Vec::default(),
 	));
 
 	let (network, system_rpc_tx, network_starter) =
@@ -431,6 +432,7 @@ pub fn new_light(mut config: Configuration) -> Result<TaskManager, ServiceError>
 	let warp_sync = Arc::new(sc_finality_grandpa::warp_proof::NetworkProvider::new(
 		backend.clone(),
 		grandpa_link.shared_authority_set().clone(),
+		Vec::default(),
 	));
 
 	let (network, system_rpc_tx, network_starter) =
